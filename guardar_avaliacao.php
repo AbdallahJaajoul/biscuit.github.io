d9305c17d0ce78e983f8fbd44277a6e3632b26fa
Cafetaria <abdullahmahercacul@gmail.com>
2025-11-10 21:06:58 +0000

    Update guardar_avaliacao.php

--- guardar_avaliacao.php
+++ guardar_avaliacao.php
@@ -1,1 +1,11 @@
+<?php
+$nome = htmlspecialchars($_POST['nome']);
+$comentario = htmlspecialchars($_POST['comentario']);
+$avaliacao = intval($_POST['avaliacao']);
 
+$linha = "$nome|$comentario|$avaliacao\n";
+file_put_contents("avaliacoes.txt", $linha, FILE_APPEND);
+
+header("Location: avaliacoes.html");
+exit;
+?>
